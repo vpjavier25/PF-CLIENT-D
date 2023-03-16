ddfdee348b260f6cca7b2a69d68709870bad19f8
Javier Vasquez <vp.javier25@gmail.com>
2023-03-16 11:33:54 -0500

    feat(client/redux): add redux toolkit logic

--- client/src/Components/CardContainer/CardContainer.jsx
+++ client/src/Components/CardContainer/CardContainer.jsx
@@ -1,7 +1,18 @@
 import { project } from '../../Utils/seed';
 import Card from '../Card/Card';
+import {useDispatch, useSelector} from 'react-redux';
+import { useEffect } from 'react';
+import { getAllProjects } from '../../Redux/Slicers/projectSlicer';
 
 export default function CardContainer() {
+    const dispatch = useDispatch();
+    const alt = useSelector(state => state.project.AllProjects);
+    const altStatus = useSelector (state => state.project.status);
+
+    useEffect(()=>{
+        if (altStatus === '') dispatch(getAllProjects());
+    }, [altStatus, dispatch])
+
     return (
         <div>
             {project.map(project => {
@@ -12,6 +23,9 @@ export default function CardContainer() {
                     image={project.images}
                 />
             })}
+
+        <span>{alt.id}</span>
+
         </div>
     )
 } 
