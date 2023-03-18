a62c2cc7bdb1920667578caa8e969a2555ebf3fe
Daniel Burgos <dburgos.developer@gmail.com>
2023-03-18 12:50:53 -0500

    refactor(From) Hooks implimentation

--- client/src/Pages/FormProjects/Errors.jsx
+++ client/src/Pages/FormProjects/Errors.jsx
@@ -1,31 +1,9 @@
-export function validations(input) {
-    let errors = {};
+import * as yup from 'yup'
 
-    if(!input.name){
-        errors.name ='Please enter a name';
-    }else if(input.name.search(/^[a-zA-Z\s]*$/) ){
-        errors.name ='It cant have numbers or symbols'
-    }
-
-    if(!input.title){
-        errors.title ='Please enter a title';
-    }else if(input.title.search(/^[a-zA-Z\s]*$/) ){
-        errors.title ='It cant have numbers or symbols'
-    }
-
-    if(!input.description){
-        errors.description ='Please enter a description';
-    }
-
-    if(input.image.search(/(https?:\/\/.*\.(?:png|jpg))/) ){
-        errors.image ='It have to be an URL'
-    }
-    
-    if(!input.location){
-        errors.location ='Please enter a location';
-    }else if(input.location.search(/^[a-zA-Z\s]*$/) ){
-        errors.location ='It cant have numbers or symbols'
-    }
-
-    return errors;
-}
+export const projectSchema = yup.object({
+    name: yup.string().matches(/^[^0-9()]+$/, { message: "not numbers allowed", excludeEmptyString: true }).required(),
+    title: yup.string().matches(/^[A-Za-z0-9]+$/, { message: "not allowed special characters", excludeEmptyString: true }).required(),
+    image: yup.string().url(),
+    location: yup.string().required(),
+    description: yup.string().matches(/[\S\s]{50,}/, "debe ingresar una descripcion de mas de 50 caracteres")
+});
--- client/src/Pages/FormProjects/FormProjects.jsx
+++ client/src/Pages/FormProjects/FormProjects.jsx
@@ -1,133 +1,65 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { postProject } from "../../Redux/Slicers/projectSlicer"
-import { validations } from "./Errors";
-//import { useForm } from 'react-hook-form';
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Button, Input, Container, VStack, Textarea, Center } from "@chakra-ui/react";
+import { projectSchema } from "./Errors";
+import { postProject } from "../../Redux/Slicers/projectSlicer";
 
 export default function FormProjects() {
-  const dispatch = useDispatch();
-  const [errors, setErrors] = useState({});
+  
+  const dispatch = useDispatch()
+  const { register, handleSubmit, formState: { errors }, watch } = useForm({
+    resolver: yupResolver(projectSchema)
+  });
 
-  const [input, setInput] = useState({
-        name: "",
-        title: "",
-        description: "",
-        image: "",
-        location: "",
-      });
-    
-  useEffect(() => {
-              
-    },[]);
-        
-    function handleSubmit(e) {
-            e.preventDefault();
-            let projectCreated = {
-              name: input.name,
-              title: input.title,
-              description: input.description,
-              image: input.image,
-              deleted: false,
-              location: input.location
-            }
-            console.log(projectCreated);
-            dispatch(postProject(projectCreated));
-            setInput({
-              name: "",
-              title: "",
-              description: "",
-              image: "",
-              location: "",
-            })
-            alert('Project successfully created')
-    }
-    
-    function handleChange(e) {
-        setInput({
-          ...input,
-          [e.target.name]: e.target.value
-        });
-        setErrors(
-          validations({
-            ...input,
-            [e.target.name]: e.target.value
-          })
-        )
-    }
+  const Submit = (data) => {
+    dispatch(postProject(data))
+  }
+
 
   return (
-    <div>
-      <br />
-      <h2>
-        Here you can submit your projects so that we can help your community
-      </h2>
-      <br />
-      <form>
-        <div>
-          <label>Name of the project: </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Here goes the name"
-            value={input.name}
-            onChange={(e) => handleChange(e)}
-          /> {errors.name && (<p>{errors.name}</p>)}
-        </div>
-        <br />
-        <div>
-          <label>Title of the project: </label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Here goes the title"
-            value={input.title}
-            onChange={(e) => handleChange(e)}
-          /> {errors.title && (<p>{errors.title}</p>)}
-        </div>
-        <br />
-        <div>
-          <label>Description of the project: </label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Here goes the description"
-            value={input.description}
-            onChange={(e) => handleChange(e)}
-          /> {errors.description && (<p>{errors.description}</p>)}
-        </div>
-        <br />
-        <div>
-          <label>Image of the project: </label>
-          <input
-            type="text"
-            name="image"
-            placeholder="Here goes the URL"
-            value={input.image}
-            onChange={(e) => handleChange(e)}
-          /> {errors.image && (<p>{errors.image}</p>)}
-        </div>
-        <br />
-        <div>
-          <label>Location of the project: </label>
-          <input
-            type="text"
-            name="location"
-            placeholder="Here goes the location"
-            value={input.location}
-            onChange={(e) => handleChange(e)}
-          /> {errors.location && (<p>{errors.location}</p>)}
-        </div>
-        <br />
-         <div>
-            {       
-              (!input.name || !input.title || !input.description || !input.image || !input.location)
-              || (errors.name || errors.title || errors.description || errors.image || errors.location) 
-              ? null : <button type="submit" onClick={(e) => handleSubmit(e)}> Upload proposal </button>
-                }
-          </div>
+    <Container>
+      <form onSubmit={handleSubmit(Submit)}>
+        <VStack spacing="24px">
+          <FormControl isInvalid={errors.name ? true : false}>
+            <FormLabel>Name</FormLabel>
+            <Input type="text" placeholder="Enter the porject's name" {...register('name')} />
+            {!errors.name ? null :<FormErrorMessage>{errors.name?.message}</FormErrorMessage>}
+          </FormControl>
+
+          <FormControl isInvalid={errors.title ? true : false}>
+            <FormLabel>Title</FormLabel>
+            <Input type='text' placeholder="Enter a title you want the contributors see" {...register('title')} />
+            {!errors.name ? null :<FormErrorMessage>{errors.title?.message}</FormErrorMessage>}
+          </FormControl>
+
+          <FormControl isInvalid={errors.image? true : false}>
+            <FormLabel>Image</FormLabel>
+            <Input type='text' placeholder="Enter a url of your location" {...register('image')} />
+            {!errors.name ? null :<FormErrorMessage>{errors.image?.message}</FormErrorMessage>}
+          </FormControl>
+
+          <FormControl isInvalid={errors.location? true : false}>
+            <FormLabel>Location</FormLabel>
+            <Input type='text' placeholder="Share your project's address" {...register('location')} />
+            {!errors.name ? null :<FormErrorMessage>{errors.location?.message}</FormErrorMessage>}
+          </FormControl>
+
+          <FormControl isInvalid={errors.description? true : false}>
+            <FormLabel>Description</FormLabel>
+            <Textarea resize="vertical" h="200px" placeholder="Share your story" {...register('description')} />
+            {!errors.name ? null :<FormErrorMessage>{errors.description?.message}</FormErrorMessage>}
+          </FormControl>
+
+          <Button type="submit" colorScheme="blue"> send </Button>
+        </VStack>
+
       </form>
-    </div>
-  );
+    </Container>
+
+  )
 }
+
 
 //Solo el achivo index de Pages recibe este export
