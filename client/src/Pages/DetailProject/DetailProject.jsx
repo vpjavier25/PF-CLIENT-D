725aa777f8b99c11cc3bc1634f4106b268104334
unknown <juantraina98@gmail.com>
2023-03-18 20:36:16 -0300

    Styles: se agregaron estilos al componente de details

--- client/src/Pages/DetailProject/DetailProject.jsx
+++ client/src/Pages/DetailProject/DetailProject.jsx
@@ -1,7 +1,8 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getProjectById, provGetId, cleanId } from "../../Redux/Slicers/projectSlicer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Box, Text, Button, Image, Center, VStack, Heading, Card } from '@chakra-ui/react'
 
 export default function DetailProject() {
   const dispatch = useDispatch();
@@ -16,18 +17,30 @@ export default function DetailProject() {
   }, []);
 
   return (
-    <div>
+    <Box maxH='1.5'>
       {Object.keys(projectById).length > 0 ? (
-        <div>
-          <img src={projectById.image} alt={projectById.name} />
-          <h2>Name: {projectById.name}</h2>
-          <h3>Title: {projectById.title}</h3>
-          <p>description: {projectById.description}</p>
-          <p>Location: {projectById.location}</p>
-        </div>
+        <Box textAlign='justify'>
+          <VStack spacing={3}>
+            <Heading as='b' fontSize='4rem' margin='1rem 3rem'>{projectById.name.toUpperCase()}</Heading>
+            <Center>
+              <Image boxSize='700px' objectFit='cover' borderRadius='full' align='center' src={projectById.images} alt={projectById.name} />
+            </Center>
+            <Card bg='#F6F8FA' variant='outline' borderColor='#D8DEE4' maxW='70rem'>
+              <Text fontSize='20px' padding='15px' >{projectById.description}</Text>
+            </Card>
+            <Card bg='#F6F8FA' variant='outline' borderColor='#D8DEE4' maxW='20rem' padding='0.5rem'>
+              <Text>{projectById.location}</Text>
+            </Card>
+          </VStack>
+        </Box>
       ) : (
-        <p>Loading..</p>
+        <Text>Loading..</Text>
       )}
-    </div>
+      <Box>
+        <Link to='/projects'>
+            <Button margin='1rem 3rem' _hover={{background:'#C9EEFF'}}>Back</Button>
+        </Link>
+      </Box>
+    </Box>
   );
 }
