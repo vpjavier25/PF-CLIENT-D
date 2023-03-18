2ea537ec2d347a84b66ed36339d446dfc8c520b7
Javier Vasquez <vp.javier25@gmail.com>
2023-03-18 16:35:27 -0500

    feat(userLogin-userForm):add a user login and user create form

--- client/src/Pages/FormProjects/FormProjects.jsx
+++ client/src/Pages/FormProjects/FormProjects.jsx
@@ -2,12 +2,12 @@ import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup"
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Button, Input, Container, VStack, Textarea, Center } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormErrorMessage, Button, Input, Container, VStack, Textarea, Center, Heading } from "@chakra-ui/react";
 import { projectSchema } from "./Errors";
 import { postProject } from "../../Redux/Slicers/projectSlicer";
 
 export default function FormProjects() {
-  
+
   const dispatch = useDispatch()
   const { register, handleSubmit, formState: { errors }, watch } = useForm({
     resolver: yupResolver(projectSchema)
@@ -19,37 +19,38 @@ export default function FormProjects() {
 
 
   return (
-    <Container>
+    <Container mt="100px" mb = "100px">
+      <Heading>Create a Project</Heading>
       <form onSubmit={handleSubmit(Submit)}>
         <VStack spacing="24px">
           <FormControl isInvalid={errors.name ? true : false}>
             <FormLabel>Name</FormLabel>
             <Input type="text" placeholder="Enter the porject's name" {...register('name')} />
-            {!errors.name ? null :<FormErrorMessage>{errors.name?.message}</FormErrorMessage>}
+            {!errors.name ? null : <FormErrorMessage>{errors.name?.message}</FormErrorMessage>}
           </FormControl>
 
           <FormControl isInvalid={errors.title ? true : false}>
             <FormLabel>Title</FormLabel>
             <Input type='text' placeholder="Enter a title you want the contributors see" {...register('title')} />
-            {!errors.name ? null :<FormErrorMessage>{errors.title?.message}</FormErrorMessage>}
+            {!errors.name ? null : <FormErrorMessage>{errors.title?.message}</FormErrorMessage>}
           </FormControl>
 
-          <FormControl isInvalid={errors.image? true : false}>
+          <FormControl isInvalid={errors.image ? true : false}>
             <FormLabel>Image</FormLabel>
             <Input type='text' placeholder="Enter a url of your location" {...register('image')} />
-            {!errors.name ? null :<FormErrorMessage>{errors.image?.message}</FormErrorMessage>}
+            {!errors.name ? null : <FormErrorMessage>{errors.image?.message}</FormErrorMessage>}
           </FormControl>
 
-          <FormControl isInvalid={errors.location? true : false}>
+          <FormControl isInvalid={errors.location ? true : false}>
             <FormLabel>Location</FormLabel>
             <Input type='text' placeholder="Share your project's address" {...register('location')} />
-            {!errors.name ? null :<FormErrorMessage>{errors.location?.message}</FormErrorMessage>}
+            {!errors.name ? null : <FormErrorMessage>{errors.location?.message}</FormErrorMessage>}
           </FormControl>
 
-          <FormControl isInvalid={errors.description? true : false}>
+          <FormControl isInvalid={errors.description ? true : false}>
             <FormLabel>Description</FormLabel>
             <Textarea resize="vertical" h="200px" placeholder="Share your story" {...register('description')} />
-            {!errors.name ? null :<FormErrorMessage>{errors.description?.message}</FormErrorMessage>}
+            {!errors.name ? null : <FormErrorMessage>{errors.description?.message}</FormErrorMessage>}
           </FormControl>
 
           <Button type="submit" colorScheme="blue"> send </Button>
