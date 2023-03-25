50985c723d301b3363455befdfadeb4a7ee30169
AlanSilcher <silcheralan@gmail.com>
2023-03-24 23:06:54 -0300

    Link a plataforma de pago

--- client/src/Redux/Store/index.js
+++ client/src/Redux/Store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectReducer from '../Slicers/projectSlicer';
 import usersSlicer from '../Slicers/userSlicer';
+import paymentSlicer from '../Slicers/paymentSlicer';
 
 export const Store = configureStore({
     reducer: {
         project: projectReducer,
-        user: usersSlicer
+        user: usersSlicer,
+        paymentLink: paymentSlicer
     }
 });
 
