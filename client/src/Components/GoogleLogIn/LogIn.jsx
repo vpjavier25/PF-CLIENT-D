fedf66abd842775e414a0c6b1a15080a61750a37
Javier Vasquez <vp.javier25@gmail.com>
2023-03-31 00:58:20 -0500

    api url

--- client/src/Components/GoogleLogIn/LogIn.jsx
+++ client/src/Components/GoogleLogIn/LogIn.jsx
@@ -4,7 +4,7 @@ import { ImGoogle } from "react-icons/im";
 export default function SignUp() {
   return (
     <div>
-      <a className="enlace" href="http://localhost:3001/auth/google">
+      <a className="enlace" href="https://lf-api-d-production.up.railway.app/auth/google">
         <Button>
           <ImGoogle color="#07588a" />
           &nbsp;&nbsp;inicio de sesion con google
